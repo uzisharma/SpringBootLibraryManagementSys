6d5adc10902e4801fa3dad6953fa469e72e0b840
Ujjwal <ujjwalsharma.ece@gmail.com>
2025-07-21 19:21:22 +0530

    savebook method completed

--- src/main/java/com/jsp/bms/controller/BookController.java
+++ src/main/java/com/jsp/bms/controller/BookController.java
@@ -2,8 +2,8 @@ package com.jsp.bms.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.jsp.bms.entity.Book;
@@ -12,15 +12,13 @@ import com.jsp.bms.repository.BookRepository;
 @Controller
 public class BookController {
 	@Autowired
-	BookRepository br;
+	BookRepository br; //creating the BookRepository interface object as it contains all the DAO methods, like save()
 	
 	@ResponseBody
-	@RequestMapping("/savebook")
-	public String saveBook(@RequestBody Book book) {
-		br.save(book);
-		System.out.println(book);
-		System.out.println("save book method invoked " + book.getTitle() +" "+ book.getId());
-		return "Book Saved Successfully";
+	@GetMapping("/savebook")
+	public Book saveBook(@RequestBody Book book) {
+		Book savedBook = br.save(book); //we will return the same object back to the postman
+		return savedBook;
 	}
 
 }
