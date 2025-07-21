6bc2d034385205124e9650a49224be39a1f5484d
Ujjwal <ujjwalsharma.ece@gmail.com>
2025-07-21 18:28:57 +0530

    entity class name book added

--- src/main/java/com/jsp/bms/controller/BookController.java
+++ src/main/java/com/jsp/bms/controller/BookController.java
@@ -2,13 +2,15 @@ package com.jsp.bms.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BookController {
 	
+	
 	@RequestMapping("/savebook")
-	public String saveBook() {
-		System.out.println("save book method invoked");
+	public String saveBook(@RequestParam String title, @RequestParam int id) {
+		System.out.println("save book method invoked " + title +" "+ id);
 		return "Book Saved Successfully";
 	}
 
---
+++ src/main/java/com/jsp/bms/entity/Book.java
@@ -0,0 +1,66 @@
+package com.jsp.bms.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Book {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String title;
+	private String author;
+	private double price;
+	private int qty;
+	private String des;
+	
+	
+	
+	@Override
+	public String toString() {
+		return "Book [id=" + id + ", title=" + title + ", author=" + author + ", price=" + price + ", qty=" + qty
+				+ ", des=" + des + "]";
+	}
+
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public int getQty() {
+		return qty;
+	}
+	public void setQty(int qty) {
+		this.qty = qty;
+	}
+	public String getDes() {
+		return des;
+	}
+	public void setDes(String des) {
+		this.des = des;
+	}
+	
+	
+}
